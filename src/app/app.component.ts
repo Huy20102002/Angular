9df2bc59740f6cad30e28004e682949b523cee73
Huy20102002 <huy.dev.2010@gmail.com>
2022-03-09 09:47:23 +0700

    daycode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'huybtph14352';
+     heroes: Array<object> =[
+       {
+         
+       }
+     ];
+     formHero: object ={
+       code: "",
+       name: "",
+       avatar: "",
+       gender: ""
+     };
 }
